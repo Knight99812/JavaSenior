50a0a7baed9556f7d9aa28b73fc090440ecc7c81
shihaojie <shihaojie@atguigu.com>
2023-02-06 09:54:10 +0800

    first commit

---
+++ git-test/src/main/java/git/GitTest.java
@@ -0,0 +1,13 @@
+package git;
+
+/**
+ * @author SHJ
+ * @create 2023-02-06 9:53
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
